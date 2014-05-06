9349cdedb7cc20c416830980129c399ecaa2f53c
Satyam Ghodasara <sghodas@gmail.com>
2014-05-06 18:06:10 -0400

    Added font switching when tapping on output

--- Overlayer Calculator/SGViewController.m
+++ Overlayer Calculator/SGViewController.m
@@ -9,7 +9,15 @@
 #import "SGViewController.h"
 
 
+static NSString * const kSGFontGillSans = @"GillSans";
+static NSString * const kSGFontGillSansCustom = @"Gill Sans Custom";
+
+static CGFloat const kSGFontSize = 52.0f;
+
+
 @interface SGViewController ()
+
+@property (readwrite, weak, nonatomic) IBOutlet UILabel *outputLabel;
 
 @end
 
@@ -25,6 +33,17 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UI Actions
+
+- (IBAction)switchFonts:(id)sender
+{
+    if ([self.outputLabel.font.fontName isEqualToString:kSGFontGillSans]) {
+        self.outputLabel.font = [UIFont fontWithName:kSGFontGillSansCustom size:kSGFontSize];
+    } else {
+        self.outputLabel.font = [UIFont fontWithName:kSGFontGillSans size:kSGFontSize];
+    }
 }
 
 @end
