96f82a8527a4cfc5d134ecace2d5ac97ba401702
Satyam Ghodasara <sghodas@gmail.com>
2014-05-06 18:23:31 -0400

    Added methods to handle numerical and decimal point input, and enabled the AC button

--- Overlayer Calculator/SGViewController.m
+++ Overlayer Calculator/SGViewController.m
@@ -19,6 +19,8 @@ static CGFloat const kSGFontSize = 52.0f;
 
 @property (readwrite, weak, nonatomic) IBOutlet UILabel *outputLabel;
 
+@property (readwrite, assign, nonatomic) double inputA, inputB;
+
 @end
 
 @implementation SGViewController
@@ -37,12 +39,36 @@ static CGFloat const kSGFontSize = 52.0f;
 
 #pragma mark - UI Actions
 
-- (IBAction)switchFonts:(id)sender
+- (IBAction)switchFonts:(UITapGestureRecognizer *)tap
 {
     if ([self.outputLabel.font.fontName isEqualToString:kSGFontGillSans]) {
         self.outputLabel.font = [UIFont fontWithName:kSGFontGillSansCustom size:kSGFontSize];
     } else {
         self.outputLabel.font = [UIFont fontWithName:kSGFontGillSans size:kSGFontSize];
+    }
+}
+
+- (IBAction)selectAllClear:(UIButton *)allClearbutton
+{
+    self.outputLabel.text = @"";
+    self.inputA = 0;
+    self.inputB = 0;
+}
+
+- (IBAction)selectNumber:(UIButton *)numberButton
+{
+    self.outputLabel.text = [self.outputLabel.text stringByAppendingString:numberButton.titleLabel.text];
+}
+
+- (IBAction)selectDecimalPoint:(UIButton *)decimalPointButton
+{
+    //  Only add a decimal point if there isn't one in the output already
+    if ([self.outputLabel.text rangeOfString:@"."].location == NSNotFound) {
+        if ([self.outputLabel.text isEqualToString:@""]) {
+            self.outputLabel.text = @"0.";
+        } else {
+            self.outputLabel.text = [self.outputLabel.text stringByAppendingString:@"."];
+        }
     }
 }
 
