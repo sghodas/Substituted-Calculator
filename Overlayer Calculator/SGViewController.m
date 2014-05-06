bcaaf283d23c7f2961e38e2de90c54f19090087f
Satyam Ghodasara <sghodas@gmail.com>
2014-05-06 19:04:36 -0400

    Added addition ability

--- Overlayer Calculator/SGViewController.m
+++ Overlayer Calculator/SGViewController.m
@@ -14,12 +14,26 @@ static NSString * const kSGFontGillSansCustom = @"Gill Sans Custom";
 
 static CGFloat const kSGFontSize = 52.0f;
 
+typedef NS_ENUM(NSInteger, SGOperation) {
+    SGOperationNone,
+    SGOperationAdd,
+    SGOperationSubstract,
+    SGOperationMultiply,
+    SGOperationDivide,
+    SGOperationExponent,
+    SGOperationRoot,
+    SGOperationLog,
+    SGOperationNaturalLog
+};
+
 
 @interface SGViewController ()
 
-@property (readwrite, weak, nonatomic) IBOutlet UILabel *outputLabel;
+@property (readwrite, weak, nonatomic) IBOutlet UILabel *displayLabel;
 
 @property (readwrite, assign, nonatomic) double inputA, inputB;
+@property (readwrite, assign, nonatomic) SGOperation operation;
+@property (readwrite, assign, nonatomic) BOOL clearOnNextInput;
 
 @end
 
@@ -29,6 +43,9 @@ static CGFloat const kSGFontSize = 52.0f;
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.operation = SGOperationNone;
+    self.clearOnNextInput = NO;
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,35 +58,138 @@ static CGFloat const kSGFontSize = 52.0f;
 
 - (IBAction)switchFonts:(UITapGestureRecognizer *)tap
 {
-    if ([self.outputLabel.font.fontName isEqualToString:kSGFontGillSans]) {
-        self.outputLabel.font = [UIFont fontWithName:kSGFontGillSansCustom size:kSGFontSize];
+    if ([self.displayLabel.font.fontName isEqualToString:kSGFontGillSans]) {
+        self.displayLabel.font = [UIFont fontWithName:kSGFontGillSansCustom size:kSGFontSize];
     } else {
-        self.outputLabel.font = [UIFont fontWithName:kSGFontGillSans size:kSGFontSize];
+        self.displayLabel.font = [UIFont fontWithName:kSGFontGillSans size:kSGFontSize];
     }
 }
 
 - (IBAction)selectAllClear:(UIButton *)allClearbutton
 {
-    self.outputLabel.text = @"";
+    self.displayLabel.text = @"";
     self.inputA = 0;
     self.inputB = 0;
 }
 
 - (IBAction)selectNumber:(UIButton *)numberButton
 {
-    self.outputLabel.text = [self.outputLabel.text stringByAppendingString:numberButton.titleLabel.text];
+    if (self.clearOnNextInput) {
+        [self resetInput];
+        self.clearOnNextInput = NO;
+    }
+    self.displayLabel.text = [self.displayLabel.text stringByAppendingString:numberButton.titleLabel.text];
+}
+
+- (IBAction)selectOperation:(UIButton *)operationButton
+{
+    BOOL continousOperation = YES;
+    if (self.operation == SGOperationNone) {
+        //  Read and clear the current input
+        self.inputA = [self currentInput];
+        self.clearOnNextInput = YES;
+        continousOperation = NO;
+    }
+    
+    if ([operationButton.titleLabel.text isEqualToString:@"+"]) {
+        self.operation = SGOperationAdd;
+    } else if ([operationButton.titleLabel.text isEqualToString:@"−"]) {
+        self.operation = SGOperationSubstract;
+    } else if ([operationButton.titleLabel.text isEqualToString:@"×"]) {
+        self.operation = SGOperationMultiply;
+    } else if ([operationButton.titleLabel.text isEqualToString:@"÷"]) {
+        self.operation = SGOperationDivide;
+    } else if ([operationButton.titleLabel.text isEqualToString:@"log"]) {
+        self.operation = SGOperationLog;
+    } else if ([operationButton.titleLabel.text isEqualToString:@"ln"]) {
+        self.operation = SGOperationNaturalLog;
+    } else if ([operationButton.titleLabel.text isEqualToString:@"x^y"]) {
+        self.operation = SGOperationExponent;
+    } else if ([operationButton.titleLabel.text isEqualToString:@"y√x"]) {
+        self.operation = SGOperationRoot;
+    }
+    
+    if (continousOperation) {
+        [self evaluate:nil];
+    }
+    
 }
 
 - (IBAction)selectDecimalPoint:(UIButton *)decimalPointButton
 {
     //  Only add a decimal point if there isn't one in the output already
-    if ([self.outputLabel.text rangeOfString:@"."].location == NSNotFound) {
-        if ([self.outputLabel.text isEqualToString:@""]) {
-            self.outputLabel.text = @"0.";
+    if ([self.displayLabel.text rangeOfString:@"."].location == NSNotFound) {
+        if ([self.displayLabel.text isEqualToString:@""]) {
+            self.displayLabel.text = @"0.";
         } else {
-            self.outputLabel.text = [self.outputLabel.text stringByAppendingString:@"."];
+            self.displayLabel.text = [self.displayLabel.text stringByAppendingString:@"."];
         }
     }
+}
+
+- (IBAction)selectNegate:(UIButton *)negateButton
+{
+    double currentInput = [self.displayLabel.text doubleValue];
+    if (currentInput != 0) {
+        self.displayLabel.text = [@(currentInput*-1) stringValue];
+    }
+}
+
+- (IBAction)evaluate:(UIButton *)evaluateButton
+{
+    //  Read and clear the current input
+    self.inputB = [self currentInput];
+    [self resetInput];
+    
+    double result;
+    switch (self.operation) {
+        case SGOperationAdd:
+            result = self.inputA + self.inputB;
+            break;
+            
+        case SGOperationSubstract:
+            break;
+        
+        case SGOperationMultiply:
+            break;
+        
+        case SGOperationDivide:
+            break;
+            
+        case SGOperationExponent:
+            break;
+        
+        case SGOperationRoot:
+            break;
+            
+        case SGOperationLog:
+            break;
+            
+        case SGOperationNaturalLog:
+            break;
+            
+        case SGOperationNone:
+            break;
+    }
+    
+    self.displayLabel.text = [@(result) stringValue];
+    
+    //  Reset state
+    self.operation = SGOperationNone;
+    self.inputA = result;
+    self.inputB = 0;
+}
+
+#pragma mark - Helpers
+
+- (double)currentInput
+{
+    return [self.displayLabel.text doubleValue];
+}
+
+- (void)resetInput
+{
+    self.displayLabel.text = @"";
 }
 
 @end
